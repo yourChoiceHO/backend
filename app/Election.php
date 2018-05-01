459d600599744e4f5d7e6fbcabb97d671c607949
Unknown <planlos1984@gmx.de>
2018-05-01 12:14:11 +0200

    just try it

---
+++ app/Election.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Election extends Model
+{
+    public $timestamps = false;
+
+    protected $table = 'elections';
+
+    protected $fillable = ['typ', 'text', 'start_date', 'end_date', 'state'];
+    protected $guarded = ['id_election', 'client_id'];
+
+
+    // DEFINE RELATIONSHIPS --------------------------------------------------
+
+    //Eine Wahl MUSS GENAU EINEN client haben
+    /*
+    public function client() {
+        return $this->belongsTo('App\Client');
+    }
+    */
+
+    //Eine election KANN MEHRERE parties haben
+    //trägt meine id als FK in party ein
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function parties() {
+        return $this->hasMany('App\Party');
+    }
+
+
+
+
+}
---
+++ app/Party.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Election;
+
+class Party extends Model
+{
+    //es werden keine timestamps benötigt
+    //public $timestamps = false;
+
+    //Abbildung DB-Entität <-> Klasse
+    //Klasse parties nutzt Entität Party
+    //@var string  -> was bedeutet das, bzw. für was ist es da?
+    protected $table = 'parties';
+
+    //Spalten auf die zugegriffen werden darf
+    protected $fillable = ['name', 'text', 'consituency', 'vote'];
+    //election_id und id nicht, sollen generiert werden beim erstellen
+    //und nur automatisch gesetzt werden können
+    protected $guarded = ['id', 'election_id'];
+
+
+    // DEFINE RELATIONSHIPS --------------------------------------------------
+
+    //jede Partei MUSS GENAU EINE election haben
+    //Gegenstück zu $this->hasMany('App\Party') in election
+    //erwartet, dass es in election eine id gibt,die in party dann als FK eingetragen wird
+    public function election() {
+        return $this->belongsTo('App\Election');
+    }
+
+    //jede Partei KANN KEINEN, EINEN ODER MEHRERE(???, laut DB-Modell nicht) Kandidaten haben
+    //Tabelle candidate noch nicht vorhanden
+    /*
+    public function candidates() {
+        return $this->hasMany('App\Candidate');
+    }
+    */
+
+    //FUNCTIONS-------------------------------------------------------------
+
+    /*
+    public function setElection(Election election) {
+        $this.election_id = election.election_id;
+    }
+    */
+
+    //Wip
+    //soll eigentlich die stärkste Parei zurückgeben, aber mit was vergleichen
+    public function scopePartyGTFive($query){
+        return $query->where('vote','>','5');
+    }
+
+    //
+    public function getAllNames($parties){
+        foreach($parties as $party){
+            return $party->name;
+        }
+    }
+
+}
