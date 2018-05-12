7ec8ff7ce8b657c94acff05935be554380acfe8f
Ana <agalperi@stud.hs-offenburg.de>
2018-05-12 17:57:36 +0200

    added resources candidate, client, party, referendum

---
+++ app/Http/Resources/Candidate.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Candidate extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'last_name' => $this->last_name,
+            'first_name' => $this->first_name,
+            'party_id' => $this->party_id,
+            'consituency' => $this->consituency,
+            'election_id' => $this->election_id,
+            'vote' => $this->vote,
+        ];
+    }
+}
